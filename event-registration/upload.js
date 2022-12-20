fe8d94b7de960c59537e7d4fede422affff97518
Muzain187 <muzain.ashraf@gmail.com>
2022-12-20 12:16:50 +0530

    upload btn is created

--- event-registration/upload.js
+++ event-registration/upload.js
@@ -1,38 +1,59 @@
 
 var form = document.getElementById('sheetdb-form');
+// let url = "https://script.google.com/macros/s/AKfycbxaKqaq29ZiTz7Hpo5LAJTOUG3bP0FR7eyGGOMJAKkvZfTd2QFjVxCvoAuIM4fSOxkzWQ/exec";
+let url = "https://script.google.com/macros/s/AKfycbycueg95dXr4uMkHAAGUOCQBdjeAqFck2qWiqiD9X2l3mbvL2MBlMzpGMlRGC7XlU93mg/exec";
 form.addEventListener("submit", e => {
   e.preventDefault();
-  Swal.fire({
-    title: 'Loading...',
-    allowEscapeKey: false,
-    allowOutsideClick: false,
-    showConfirmButton: false,
-    didOpen: () => {
-      Swal.showLoading();
-    }
-  });
-  // console.log(e);
-  fetch(form.action, {
+  // Swal.fire({
+  //   title: 'Loading...',
+  //   allowEscapeKey: false,
+  //   allowOutsideClick: false,
+  //   showConfirmButton: false,
+  //   didOpen: () => {
+  //     Swal.showLoading();
+  //   }
+  // });
+  let data = {
+    name:document.getElementById('name').value,
+    email:document.getElementById('email').value,
+    phone:document.getElementById('phone').value,
+    usn:document.getElementById('usn').value,
+    sem:document.getElementById('Sem').value,
+    dep:document.getElementById('Department').value,
+    event:document.getElementById('Event').value,
+    transid:document.getElementById('transid').value,
+    img:obj
+  }
+  console.log(data);
+  Swal.fire('Uploading please wait for a moment')
+  fetch(url, {
       method : "POST",
-      body: new FormData(document.getElementById("sheetdb-form")),
+      body:JSON.stringify(data)
+      // body: new FormData(document.getElementById("sheetdb-form")),
+
   }).then(
-      response => response.json()
+      response => response.text()
   ).then((html) => {
-    fetch(url,{
-      method:"POST",
-      body:JSON.stringify(obj)
-    })
-    .then(r => r.text())
-    .then(data => {
-      form.reset();
-      window.open('success.html','_self')
-    })
+    Swal.fire(
+      'Thank you for registoring',
+      'your information is successfully stored',
+      'success'
+    )
+    // fetch(url,{
+    //   method:"POST",
+    //   body:JSON.stringify(obj)
+    // })
+    // .then(r => r.text())
+    // .then(data => {
+    //   form.reset();
+    //   window.open('success.html','_self')
+    // })
   });
+  document.getElementById("sheetdb-form").reset();
 });
 
 
 let file = document.getElementById('picture');
-let url = "https://script.google.com/macros/s/AKfycbzBTDg3RjYM_iY0ZEoWVhVahlHt0LTUltoqtpepmkpgX9emuejp7Qx2f7UY3eT0DqkcQQ/exec";
 let obj;
 let res,spt;
 file.addEventListener('change',()=>{
@@ -45,9 +66,9 @@ file.addEventListener('change',()=>{
       type:file.files[0].type,
       name:file.files[0].name
     }
-    let a = JSON.stringify(obj);
-    let b = JSON.parse(a);
-    console.log(b.type)
+    // let a = JSON.stringify(obj);
+    // let b = JSON.parse(a);
+    // console.log(b.type)
   })
   fr.readAsDataURL(file.files[0]);
 })
